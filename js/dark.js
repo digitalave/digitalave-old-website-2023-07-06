5393e13800a8ac27240666dcd1e6503b3b92451e
Dimuthu_Daundasekara <digitalavenuelk@gmail.com>
2021-01-30 11:49:25 +0530

    A

--- js/dark.js
+++ js/dark.js
@@ -1,18 +1,3 @@
-var checkbox = document.querySelector('input[name=mode]');
-
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light')
-    }
-})
-
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition');
-    }, 1000)
-}
+$(".theme-switch").on("click", () => {
+    $("body").toggleClass("light-theme");
+  });
